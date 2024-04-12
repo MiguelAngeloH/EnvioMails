57406f091a19ab306cb950d663c4f42a682f3f0f
Mikel Hinojosa <boras728@hotmail.com>
2024-04-12 17:01:45 -0600

    agregamos el spinner 12 abril

--- js/app.js
+++ js/app.js
@@ -1,36 +1,93 @@
 document.addEventListener('DOMContentLoaded',function() {
+          const email={
+            email:'',
+            asunto:'',
+            mensaje:''
+          }
+         // console.log(email);
+
   
   //selecionar los elementos de la interfaz
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
     const formulario = document.querySelector('#formulario');
+    const btnSubmit = document.querySelector('#formulario button[type = "submit"]');
+    const btnReset = document.querySelector('#formulario button[type = "reset"]');
+    const spiners = document.querySelector('#spiners');
 
    
 
     //asignar eventos
-    inputEmail.addEventListener('blur',validar);  
-    inputMensaje.addEventListener('blur', validar);
-    inputAsunto.addEventListener('blur', validar);
-    inputEmail.addEventListener('focus', validar);
+    inputEmail.addEventListener(  'input',validar);  
+    inputAsunto.addEventListener( 'input', validar);
+    inputMensaje.addEventListener('input', validar);
+    
+    
+
+    btnReset.addEventListener( 'click',function(e){
+      e.preventDefault();
+      
+      email.email='';
+      email.asunto='';
+      email.mensaje='';
+      
+      formulario.reset();
+      comprobarEmail();
+
+});
+
+          function enviarEmail(e){
+              e.preventDefault();
+             // console.log('enviando...');
+             spiners.classList.add('flex');
+             spiners.classList.add('hidden');
+             
+             setTimeout(() => {
+             spiners.classList.remove('flex');
+             spiners.classList.add('hidden');
+             }, 3000);
+
+
+          }
 
 
   
     //realizaamos la funcion Validar ok 
         function validar(e) {
-           // console.log(e.target.parentElement.nextElementSibling);
+          //  console.log(e.target.parentElement.nextElementSibling);
             if(e.target.value.trim() === ''){
-                //console.log('Esta vacio el Campo ');
-                mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);   
+               // console.log('Esta vacio el Campo ');
+                mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
+                email[e.target.name]='';
+                comprobarEmail();   
             return;
             }
 
             if (e.target.id ==='email' && !validarEmail(e.target.value)) {
-              mostrarAlerta(`El email no es valido`, e.target.parentElement);
+              mostrarAlerta('El email no es valido', e.target.parentElement);
+              email[e.target.name]='';
+              comprobarEmail();
               return;
             } 
+            
             limpiarAlerta(e.target.parentElement);
             //console.log("Despues del IF");
+
+            //Asignar los valores ok ok 
+            email[e.target.name] =e.target.value.trim().toLowerCase();
+            console.log(email);
+
+            //comporobar email
+            comprobarEmail();
+            
+
+
+
+
+
+
+
      }
 
      function mostrarAlerta(mensaje, referencia){
@@ -68,6 +125,20 @@ document.addEventListener('DOMContentLoaded',function() {
        const resultado = regex.test(email);
        //console.log(resultado);  
        return resultado;   
+       //usuario@dominio
 
       }
+
+          function comprobarEmail(){            
+            if (Object.values(email).includes('')) {
+              btnSubmit.classList.add('opacity-50');
+                  btnSubmit.disabled = true; 
+                  return;
+            }
+                  btnSubmit.classList.remove('opacity-50');
+                  btnSubmit.disabled = false; 
+          }
+
+
+
       });//cierra el principio ok 
